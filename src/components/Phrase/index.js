76252c225f848a31b3754212db5338f5e06d3806
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-02-17 15:24:54 -0500

    phrase

--- src/components/Phrase/index.js
+++ src/components/Phrase/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Phrase() {
   return <div className='phrase'>
     <span className='phrase__text'>
-      Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus
+    Cada servicio que ofrecemos es una oportunidad para demostrar nuestra excelencia y liderazgo. Nos enorgullece ser innovadores y pioneros en el mundo de la reparación automotriz, utilizando la última tecnología y técnicas avanzadas para superar las expectativas de nuestros clientes. 
     </span>
   </div>
 }
