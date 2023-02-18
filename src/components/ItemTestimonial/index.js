8b34029e41d9cc49f3e3cfa59bd7dddc81947fb3
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-02-17 22:10:16 -0500

    testimonials

--- src/components/ItemTestimonial/index.js
+++ src/components/ItemTestimonial/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function ItemTestimonial () {
+function ItemTestimonial ({name,profession,comment}) {
   return (
     <div className="testimonial">
-      <strong className="testimonial__name">Nulla quis lorem</strong>
-      <p>Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada.</p>
+      <p className="testimonial__comment">"{comment}"</p>
+      <div className="testimonial__author">
+        <strong className="testimonial__name">{name}</strong>
+        <span className="testimonial__profession">{profession}</span>
+      </div>
     </div>
   )
 }
--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { ItemTestimonial } from "./../ItemTestimonial/index.js" ;
 
 const testimonialsList = [
-  {name:'', comment:''}
+  {name:'María González', profession:"Gerente", comment:'La atención al cliente en esta mecánica es excepcional. Desde que llegué, me sentí en confianza con el equipo de mecánicos. Definitivamente regresaré.'},
+  {name:'Juan Pérez', profession:"Abogado", comment:'El trabajo que realizaron en mi auto fue impresionante. No solo solucionaron el problema, sino que me explicaron todo el proceso de reparación. Recomendaré esta mecánica a todos mis amigos.'},
+  {name:'Ana Torres', profession:"Dueño de tiena", comment:'Me encantó el trato personalizado que recibí en esta mecánica. Fueron muy detallistas en la inspección de mi auto y me informaron de todo lo que necesitaba ser reparado. Gracias a ellos mi auto está como nuevo.'}
 ]
-
 function Testimonials () {
   return (
     <div className="testimonials">
-      <div className="testimonials__description">
-        <h2 className="testimonials__title">Testimonials</h2>
-        <p className="testimonials__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio velit aut blanditiis vitae iste quam nemo eaque doloribus maxime, laudantium, voluptate fuga iure ad, dignissimos rem magnam! Quas, eveniet cumque.</p>
-      </div>
       <div className="testimonials__container">
         {
           testimonialsList.map((testimonial, index) => (
-            <ItemTestimonial key={index}/>
+            <ItemTestimonial key={index} name={testimonial.name} profession={testimonial.profession} comment={testimonial.comment} />
           ))
         }
       </div>
