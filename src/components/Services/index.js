3509cc00f621d1e2964f80c0c40e547e3a8e2d2b
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-02-23 11:24:31 -0500

    change images to .webp

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { ItemService } from './../ItemService/index.js'
 
 const servicesList = [
-  {img:'./img/1.jpeg', title:"Menús personalizados", description:'Crearé un menú personalizado para tu evento o reunión que satisfaga tus gustos y necesidades. '},
-  {img:'./img/2.jpeg', title:"Catering para eventos", description:'Proporcionaré comida deliciosa y presentable para tus eventos especiales, desde cenas elegantes hasta comidas casuales. '},
-  {img:'./img/3.jpeg', title:"Clases de cocina", description:'Aprenderás a cocinar deliciosos platos con mis clases interactivas y divertidas. '},
-  {img:'./img/4.jpeg', title:"Consultoría culinaria", description:'Ofrezco consultoría culinaria a restaurantes y empresas para mejorar su oferta gastronómica.'},
+  {img:'./img/1.webp', title:"Menús personalizados", description:'Crearé un menú personalizado para tu evento o reunión que satisfaga tus gustos y necesidades. '},
+  {img:'./img/2.webp', title:"Catering para eventos", description:'Proporcionaré comida deliciosa y presentable para tus eventos especiales, desde cenas elegantes hasta comidas casuales. '},
+  {img:'./img/3.webp', title:"Clases de cocina", description:'Aprenderás a cocinar deliciosos platos con mis clases interactivas y divertidas. '},
+  {img:'./img/4.webp', title:"Consultoría culinaria", description:'Ofrezco consultoría culinaria a restaurantes y empresas para mejorar su oferta gastronómica.'},
 ]
 /*
  */
