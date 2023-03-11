3563ccd499c04a01e56605e431384ebe8e7683cf
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-03-11 18:41:17 -0500

    correction to move all info to constants

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { GeneratorForm } from './../Admin/GeneratorForm'
+import { Banner } from './../components/Banner'
+import { Phrase } from './../components/Phrase'
+import { Services } from './../components/Services'
+import { Contact } from './../components/Contact'
+import { Testimonials } from './../components/Testimonials'
 import { Footer } from './../components/Footer'
+import { Faqs } from './../components/Faqs'
 import "./../css/style.css"
 import "./../css/style.sass.css"
 function App() {
   return (
     <>
-      <GeneratorForm/>
+      <Banner/>
+      <Phrase/>
+      <Services/>
+      <Contact/>
+      <Testimonials/>
+      <Faqs/>
       <Footer/>
     </>
   );
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const banner = [
-  {title: "Mantenimiento de Automóviles de Primera Clase"},
-  {description: "Descubre cómo nuestro servicio de mecánica especializada puede ayudarte a mantener tu vehículo en su mejor estado"},
-  {image: {
+const banner = {
+  title: "Mantenimiento de Automóviles de Primera Clase",
+  description: "Descubre cómo nuestro servicio de mecánica especializada puede ayudarte a mantener tu vehículo en su mejor estado",
+  image: {
     large: "./img/banner.webp",
     small: "./img/banner.webp",
-  }}
-]
+  }
+}
 
 function Banner() {
   return( 
@@ -19,7 +19,7 @@ function Banner() {
         </div>
       </div>
       <picture  className='banner__picture'>
-        <source media="(max-width: 767px)" srcSet={banner.image.small}/>
+        <source media="(max-width: 767px)" srcSet={banner.image.large}/>
         <source srcSet={banner.image.large}/>
         <img  className='banner__image' src={banner.image.large} alt={banner.title + " - banner"} />
       </picture>
